816afc6f673fad1268b32b4b35c0c5275e0a4901
Kyaw Sithu <kyawsithu@outlook.com>
2025-02-10 13:15:41 +0630

    first commit

---
+++ ImageDataItem.cs
@@ -0,0 +1,9 @@
+
+namespace AlphaX.Blazor.Components.ImageData
+{
+    public class ImageDataItem
+    {
+        public string? ImageURL { get; set; }
+        public string? FileName { get; set; }
+    }
+}
